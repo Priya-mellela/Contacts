a1c35ecae7a4dc38bb34ff526efc94afd8f40e3d
Priya-mellela <priyanka.mellela@apree.health>
2024-09-16 08:56:40 +0530

    Initial commit

---
+++ src/test/java/com/contacts/Management/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contacts.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
